24bcb2fb07b6f0ba0d2f7f3bcb8bc6d5990bbe8f
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-08-19 21:51:48 +0545

    Config file added

---
+++ backend/src/main/java/backend/backend/configuration/SecurityConfig.java
@@ -0,0 +1,10 @@
+package backend.backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+}
--- backend/src/main/java/backend/backend/repository/LoginRepo.java
+++ backend/src/main/java/backend/backend/repository/LoginRepo.java
@@ -1,6 +1,6 @@
-package backend.backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LoginRepo extends MongoRepository {
-}
+//package backend.backend.repository;
+//
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//public interface LoginRepo extends MongoRepository {
+//}
