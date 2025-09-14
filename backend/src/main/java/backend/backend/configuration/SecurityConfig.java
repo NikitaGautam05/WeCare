6d660e58ecadcd52bce79c7979b9ba13448e3907
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-09-14 22:51:15 +0545

    changes added

--- backend/src/main/java/backend/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/backend/backend/configuration/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {  //security filter chain is added to add extra rules for security in http request
         return http.build();
     }
 
