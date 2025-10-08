75d10bf54594077aa95480072f3ce795e9361b32
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-10-08 22:51:46 +0545

    Login config

--- backend/src/main/java/backend/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/backend/backend/configuration/SecurityConfig.java
@@ -3,22 +3,43 @@ package backend.backend.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity  //this turn on spring security and allow to customize security
 
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {  //security filter chain is added to add extra rules for security in http request
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //security filter chain is added to add extra rules for security in http request
+        http.csrf(cutomizer ->cutomizer.disable());
+        http.authorizeHttpRequests(request-> request.requestMatchers("/login").permitAll().anyRequest().authenticated()); //requires evervy user to be authenticated hence login
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
    @Bean
-    public AuthenticationManager authenticationManager(){
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+       return  config.getAuthenticationManager();
 
    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("nikita")
+                .password("{noop}niki") // no password encoder
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
 
 }
