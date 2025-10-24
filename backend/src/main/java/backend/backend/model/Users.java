5b1e8ec74fa2c6abf3399877c572f9c0c764521f
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-10-24 23:06:36 +0545

    model added

--- backend/src/main/java/backend/backend/model/Users.java
+++ backend/src/main/java/backend/backend/model/Users.java
@@ -1,4 +1,38 @@
 package backend.backend.model;
 
+import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Collection;
+
+@Document(collection="login")
 public class Users {
+    @Id
+    private int id;
+    private String userName;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
