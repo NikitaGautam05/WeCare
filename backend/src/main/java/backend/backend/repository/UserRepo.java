5859b206887e3be35cbc9997f2a06d1a0dc5001f
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-10-26 18:34:05 +0545

    backend

--- backend/src/main/java/backend/backend/repository/UserRepo.java
+++ backend/src/main/java/backend/backend/repository/UserRepo.java
@@ -1,10 +1,11 @@
 package backend.backend.repository;
 
 import backend.backend.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.security.core.userdetails.User;
 
-public interface UserRepo extends JpaRepository<Users, Integer> {
+public interface UserRepo extends MongoRepository<Users, Integer> {
     Users findByUserName(String username);
 
 }
