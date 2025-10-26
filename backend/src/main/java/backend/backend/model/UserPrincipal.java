973ec682feedc8e3bb2fb36f89ff56ed248875f1
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-10-26 17:43:23 +0545

    basic authentication

--- backend/src/main/java/backend/backend/model/UserPrincipal.java
+++ backend/src/main/java/backend/backend/model/UserPrincipal.java
@@ -2,45 +2,53 @@ package backend.backend.model;
 //user principal in spring means the current user who is trying to access
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 //userDetails is a inbuilt interface which is made for user authetication so when i implement it,
 //it will know that username and password are not random variable but a things user for user authetication
 public class UserPrincipal implements UserDetails {
+    private Users user;
+    public UserPrincipal(Users user) {
+        this.user=user;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")
+        );
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getUserName();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
