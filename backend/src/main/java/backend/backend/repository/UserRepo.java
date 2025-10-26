c9dcb3fa89fe0846b58313792ded61af8f4e9be8
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-10-26 17:15:08 +0545

    new model added

--- backend/src/main/java/backend/backend/repository/UserRepo.java
+++ backend/src/main/java/backend/backend/repository/UserRepo.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
 
 public interface UserRepo extends JpaRepository<Users, Integer> {
+    Users findByUserName(String username);
+
 }
