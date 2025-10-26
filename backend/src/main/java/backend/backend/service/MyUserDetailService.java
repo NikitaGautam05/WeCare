15e61b479589428816e13df8beec3a7913c6d0c4
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-10-26 17:15:08 +0545

    new model added

--- backend/src/main/java/backend/backend/service/MyUserDetailService.java
+++ backend/src/main/java/backend/backend/service/MyUserDetailService.java
@@ -1,5 +1,9 @@
 package backend.backend.service;
 
+import backend.backend.model.Users;
+import backend.backend.repository.UserRepo;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +11,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepo repo;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       Users user=repo.findByUserName(username);
+        if (user==null) {
+            System.out.println("no users found");
+            throw new UsernameNotFoundException("user not found");
+        }
         return null;
     }
 }
