1bdd017645e482b230552bab46b9bd654d14dbfc
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2025-10-29 21:13:53 +0545

    userController

--- backend/src/main/java/backend/backend/controller/UserController.java
+++ backend/src/main/java/backend/backend/controller/UserController.java
@@ -1,9 +1,17 @@
 package backend.backend.controller;
 
+import backend.backend.service.MyUserDetailService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class UserController {
+    MyUserDetailService userService;
+
+    @GetMapping("/details")
+        userService.getAllUser();
+
+
 
 }
