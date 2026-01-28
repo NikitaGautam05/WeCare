52168701a2cea7eb11f56800c585e024d55f8b9b
NikitaGautam05 <nikitaa.gautam01@gmail.com>
2026-01-28 22:17:38 +0545

    data fetching from db

--- backend/src/main/java/backend/backend/configuration/WebConfig.java
+++ backend/src/main/java/backend/backend/configuration/WebConfig.java
@@ -3,6 +3,7 @@ package backend.backend.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,6 +18,10 @@ public class WebConfig {
                         .allowedOrigins("http://localhost:5173") // React dev server
                         .allowedMethods("GET","POST","PUT","DELETE")
                         .allowCredentials(true);
+            }
+            public void addResourceHandlers(ResourceHandlerRegistry registry){
+                registry.addResourceHandler("/uploads/**")
+                        .addResourceLocations("file:D:/fyp demo/backend/uploads/");
             }
         };
     }
